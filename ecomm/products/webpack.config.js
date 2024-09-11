5b893109cd931b9119696e8eb43c85a7d0023e3f
dsagm7 <dsagm7>
2024-09-12 00:08:48 +0530

    add webpack plugin for htmp

--- ecomm/products/webpack.config.js
+++ ecomm/products/webpack.config.js
@@ -1,6 +1,8 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   devServer: {
     port: 5000,
   },
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
 };
