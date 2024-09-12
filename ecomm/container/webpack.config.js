0eb293f6077f388938fc9e6afbaa9e606d8b4d93
dsagm7 <dsagm7>
2024-09-12 16:23:22 +0530

    setup module federation

--- ecomm/container/webpack.config.js
+++ ecomm/container/webpack.config.js
@@ -1,8 +1,17 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugIn = require("webpack/lib/container/ModuleFederationPlugin");
 module.exports = {
   mode: "development",
   devServer: {
     port: 5001,
   },
-  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+    new ModuleFederationPlugIn({
+      name: "container",
+      remotes: {
+        products: "products@http://localhost:5000/remoteEntry.js",
+      },
+    }),
+  ],
 };
--- ecomm/products/webpack.config.js
+++ ecomm/products/webpack.config.js
@@ -1,8 +1,21 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugIn = require("webpack/lib/container/ModuleFederationPlugin");
+
 module.exports = {
   mode: "development",
   devServer: {
     port: 5000,
   },
-  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+  plugins: [
+    new ModuleFederationPlugIn({
+      name: "products",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./ProductsIndex": "./src/index",
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
