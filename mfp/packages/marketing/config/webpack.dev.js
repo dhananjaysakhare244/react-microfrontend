af88267cd95caaab36e858a04a1deab9c92b813c
dsagm7 <dsagm7>
2024-10-06 22:10:44 +0530

    Delegate shared modules

--- mfp/packages/marketing/config/webpack.dev.js
+++ mfp/packages/marketing/config/webpack.dev.js
@@ -2,6 +2,8 @@ const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
 const devConfig = {
   mode: "development",
   devServer: {
@@ -18,6 +20,7 @@ const devConfig = {
       exposes: {
         "./MarketingApp": "./src/bootstrap",
       },
+      shared: packageJson.dependencies,
     }),
   ],
 };
